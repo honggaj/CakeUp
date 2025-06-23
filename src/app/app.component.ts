9e8f600aba54245059e8ae1b49242ad0f2a450e5
Ngô Gia Bảo <ngobao0419@gmail.com>
2025-06-23 19:10:59 +0700

    reponsive web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false, // nếu bạn dùng standalone components thì để là true
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // bạn dùng .css hoặc .scss tùy
 })
 export class AppComponent {
   title = 'CakeUp';
+
+  isMenuOpen: boolean = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
